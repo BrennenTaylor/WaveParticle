5335476c590f90a36ae1b88aaa7cea30aeffc86a
Brennen Taylor <Brennen Taylor>
2017-02-21 08:22:14 -0600

    Update setup.bat with new style used in farlor

--- setup.bat
+++ setup.bat
@@ -3,4 +3,6 @@
 echo "Set up the environment for using visual studio x64 compiler"
 echo "NOTE: Make cross platform...?"
 
-call vcvarsall x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
